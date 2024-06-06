388c47ad3bd0216cd4a0d1faaffe1615c33c7276
kshitijkroger <108348633+kshitijkroger@users.noreply.github.com>
2024-06-06 08:02:53 +0530

    Update Incident.controller.js

--- src/controllers/Incident Controller/Incident.controller.js
+++ src/controllers/Incident Controller/Incident.controller.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 import { INC6028019, INC6050882, INC6070530 } from '../../constants/krogerURLConstant.js';
-
-dotenv.config();
 
 const Incident = async (req, res) => {
 
     const incidentNumber = req.params['incidentNumber'];
 
     try {
-
         if(incidentNumber === 'INC6070530')
             res.send(INC6070530);
         else if (incidentNumber === 'INC6028019')
@@ -18,7 +14,6 @@ const Incident = async (req, res) => {
             res.send(INC6050882)
         else 
             res.send({})    //Error
-
     } catch (err) {
         // console.log(err);
         res.send(err);
