7e92f9da8ab1d86700ed018cdd769d25730eba68
kshitijkroger <108348633+kshitijkroger@users.noreply.github.com>
2024-06-10 22:43:39 +0530

    Update Incident.controller.js

--- src/controllers/Incident Controller/Incident.controller.js
+++ src/controllers/Incident Controller/Incident.controller.js
@@ -7,13 +7,13 @@ const Incident = async (req, res) => {
 
     try {
         if(incidentNumber === 'INC6070530')
-            res.send(INC6070530);
+            res.send(INC6070530.result[0]);
         else if (incidentNumber === 'INC6028019')
-            res.send(INC6028019)
+            res.send(INC6028019.result[0])
         else if (incidentNumber === 'INC6050882')
-            res.send(INC6050882)
+            res.send(INC6050882.result[0])
         else 
-            res.send({})    //Error
+            res.send({statusCode: 404, statusMessage: "Incident not found"})    //Error
     } catch (err) {
         // console.log(err);
         res.send(err);
